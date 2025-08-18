76134eb9c0cef75560448e70881309cff3cc2353
ehdrb01 <kdg1941@naver.com>
2025-08-18 20:15:30 +0900

    create Image section

---
+++ fish/src/main/java/hgu/likelion/fish/commons/image/repository/S3Repository.java
@@ -0,0 +1,7 @@
+package hgu.likelion.fish.commons.image.repository;
+
+import hgu.likelion.fish.commons.image.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface S3Repository extends JpaRepository<Long, Image> {
+}
